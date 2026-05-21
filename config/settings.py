260d882bf1edf8ff657238e3ce8144223a05c119
Богдан Чебаненко <bchebanenko@gmail.com>
2026-05-21 18:15:26 +0300

    fix: remove deepgram api key. Use gemini to transcribe audio files

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+
 GOOGLE_SHEETS_ID = os.getenv("GOOGLE_SHEETS_ID")
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
 GOOGLE_SECRET_FILE = os.getenv("GOOGLE_SECRET_FILE", "google_secret.json")
