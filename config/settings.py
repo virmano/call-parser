c9a7318e33bc3cc7a743e00bab8b62e5fa8ee460
Богдан Чебаненко <bchebanenko@gmail.com>
2026-05-22 11:43:25 +0300

    feat: create list of fields with usings to make manager score

--- config/settings.py
+++ config/settings.py
@@ -8,3 +8,13 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GOOGLE_SHEETS_ID = os.getenv("GOOGLE_SHEETS_ID")
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
 GOOGLE_SECRET_FILE = os.getenv("GOOGLE_SECRET_FILE", "google_secret.json")
+
+SCORE_FIELDS = [
+    "представлення",
+    "кузов_автомобіля",
+    "рік_автомобіля",
+    "пробіг",
+    "комплексна_діагностика",
+    "попередні_роботи",
+    "прощання"
+]
